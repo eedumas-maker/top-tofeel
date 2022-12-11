7301817003855b1414388bd1c63f17b50fd1a23c
Evan <eedumas@gmail.com>
2022-12-10 16:37:29 -0800

    Add list filerting based on have felt or not

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
 class feeling {
-
     id = 0;
 
-    constructor(name, description, when, where){
+    constructor(name, description, when, where, haveFelt = false){
         this.name = name;
         this.description = description;
         this.when = when;
         this.where = where;
+        this.haveFelt = haveFelt;
     }
+    
 }
 
+// contains main list as well as all sorting logic
+
 function FeelingList() {
-   const list = [];
+   const _list = [];
 
     const addFeel = (feeling) => {
-        feeling.id = list.length; // i love me an internal id
+        feeling.id = _list.length; // i love me an internal id
 
-        list.push(feeling);
+        _list.push(feeling);
     }
 
-    const getList = () => {
-        return list;
+    const getAllFeels = () => {
+        return _list;
     }
 
-    const showFeels = () => {
-        list.forEach((feel) => {
-            console.log(feel);
+    const showHaveFelt = () => {
+        let haveFeltList = _list.filter(function (feel) {
+            return feel.haveFelt === true;
         })
+        return haveFeltList;
+    }
+
+    const showHaveNotFelt = () => {
+        let haveNotFeltList = _list.filter(function (feel){
+            return feel.haveFelt === false;
+        })
+        return haveNotFeltList;
     }
 
     return {
         addFeel,
-        getList,
-        showFeels
+        getAllFeels,
+        showHaveFelt,
+        showHaveNotFelt
     }
 }
 
 
 // hardcoded bits to be gotten rid of later ///
 const sad = new feeling("sad","kinda down","monday","work");
-const glad = new feeling("glad","kinda nice","tuesday","home");
-const mad = new feeling("mad","kinda angry","wednesday","work");
+const glad = new feeling("glad","kinda nice","tuesday","home", true);
+const mad = new feeling("mad","kinda angry","wednesday","work", true);
 
 const list = FeelingList();
 
 list.addFeel(sad);
 list.addFeel(glad);
 list.addFeel(mad);
-    
-
-function DomHandler() {
-
-    const list = FeelingList();
-    const table = document.querySelector('.list');
-
-}
