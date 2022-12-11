e127d1ec87a5ee940c050b9477af8866d2be64a4
Evan <eedumas@gmail.com>
2022-12-10 20:52:37 -0800

    Implement sort by date and change when property to date

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class feeling {
 
-    constructor(name, description, when, where, haveFelt = false){
+    // will want to add other properties later: how long delayed, priority, etc.
+    constructor(name, description, date, where, haveFelt = false){
         this.name = name;
         this.description = description;
-        this.when = when;
+        this.date= date;
         this.where = where;
         this.haveFelt = haveFelt;
     }
@@ -25,14 +26,17 @@ class feeling {
 function FeelingList() {
    const _list = [];
 
+   // just a simple push to add a feel to the list
     const addFeel = (feeling) => {
         _list.push(feeling);
     }
 
+    // lets you access the internal array
     const getAllFeels = () => {
         return _list;
     }
 
+    // uses filter function to pull out and make temp array 
     const showHaveFelt = () => {
         let haveFeltList = _list.filter(function (feel) {
             return feel.haveFelt === true;
@@ -40,6 +44,7 @@ function FeelingList() {
         return haveFeltList;
     }
 
+    // same as above
     const showHaveNotFelt = () => {
         let haveNotFeltList = _list.filter(function (feel){
             return feel.haveFelt === false;
@@ -47,6 +52,17 @@ function FeelingList() {
         return haveNotFeltList;
     }
 
+    // sort be recent, but don't alter original list
+    const sortByRecent = () => {
+        return sortedList = _list.slice().sort((a,b) => b.date - a.date);
+    }
+
+    // to add: 
+    // sort by time (recent first)
+    // sort by location
+    // 
+
+    //edits main array and removes a feel
     const removeFeel = (index) => {
         _list.splice(index, 1);
     }
@@ -56,15 +72,16 @@ function FeelingList() {
         getAllFeels,
         showHaveFelt,
         showHaveNotFelt,
+        sortByRecent,
         removeFeel
     }
 }
 
 
 // hardcoded bits to be gotten rid of later ///
-const sad = new feeling("sad","kinda down","monday","work");
-const glad = new feeling("glad","kinda nice","tuesday","home", true);
-const mad = new feeling("mad","kinda angry","wednesday","work", true);
+const sad = new feeling("sad","kinda down",new Date('2022-11-01'),"work");
+const glad = new feeling("glad","kinda nice",new Date('2022-12-09'),"home", true);
+const mad = new feeling("mad","kinda angry",new Date('2022-12-06'),"work", true);
 
 const list = FeelingList();
 
